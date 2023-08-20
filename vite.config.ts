892f99cd7e212879ed916f4e1f138fa5e42e816c
Maksym Zubko <56440333+maksymzubko@users.noreply.github.com>
2023-08-21 02:18:16 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
-dotenv.config({path: process.env.NODE_ENV === 'production' ? './.env.prod' : './env.dev'})
+// dotenv.config({path: process.env.NODE_ENV === 'production' ? './.env.prod' : './env.dev'})
+const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
-  base: env.VITE_HOME
+export default defineConfig(({command, mode})=> {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    base: env.VITE_HOME
+  }
 })
